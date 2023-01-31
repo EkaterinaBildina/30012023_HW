fa2e381b4785a869f948ab648eb03106aef8207a
EkaterinaBildina <kukresch@gmail.com>
2023-01-31 10:26:57 +0300

    Task66 formating doc

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -20,12 +20,12 @@ else Console.WriteLine("Number's type error");
 
 int SumOfNaturalNumbers(int num1, int num2)
 {
-    if (num1 < num2) return num1 + SumOfNaturalNumbers(num1+1, num2);
+    if (num1 < num2) return num1 + SumOfNaturalNumbers(num1 + 1, num2);
 
     if (num1 > num2) return num1 + SumOfNaturalNumbers(num1 - 1, num2);
 
     if (num1 == num2) return num1;
-    
+
     return num1;
 }
 
